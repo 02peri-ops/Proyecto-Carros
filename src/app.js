d4f3efee9a2d0757103ee40ac3c990b26f4f7fcd
Diana Gonzalez <dainasld7@gmail.com>
2026-01-30 17:42:14 -0600

    App ya con las rutas establecidas

--- src/app.js
+++ src/app.js
@@ -3,8 +3,21 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
+
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth.routes');
+const carRoutes = require('./routes/car.routes');
+const errorHandler = require('./middlewares/errorHandler');
+
 //Se crea la app 
 const app = express();
+
+//Conectar a la base de datos
+connectDB();
+
+//Rutas
+app.use('/api/auth', authRoutes);
+app.use('/api/cars', carRoutes);
 
 //Configuración Midelleware
 app.use(cors());
@@ -15,10 +28,8 @@ app.get('/', (req, res) => {
     res.send('¡Hola, mundo! La aplicación está funcionando correctamente.');
 });
 
-//Conexión a MongoDB
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('Conectado a MongoDB'))
-    .catch((error) => console.error('Error al conectar a MongoDB:', error));
+//Manejo de errores
+app.use(errorHandler);
 
 //Puerto desde variables de entorno
 const PORT = process.env.PORT || 3000;
